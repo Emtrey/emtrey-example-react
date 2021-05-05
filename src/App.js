cf2a012defc5706f4477a6130e8fc428fbadf542
Max Dignan <maxdignan@gmail.com>
2021-05-04 21:22:23 -0400

    0.7

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { Link } from 'react-router';
 
 class App extends React.Component {
-    render() {
-        return (
-          <div className="container">
-            <nav className="main">
-              <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
-                <li><Link to="/about">About</Link></li>
-              </ul>
-            </nav>
+  render() {
+    return (
+      <div className="container">
+        <nav className="main">
+          <ul>
+            <li><Link to="/">Home 0.7</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+        </nav>
 
-            {this.props.children}
-          </div>
-        );
-    }
+        {this.props.children}
+      </div>
+    );
+  }
 }
 
 export default App;
